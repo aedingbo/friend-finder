0c230d4689de1e4d441577cd5b3cf0a8e628220b
Anf <aedingbo@outlook.com>
2020-03-31 03:06:20 -0500

    push

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Daenerys Targaryen",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/e/ee/QueenDaenerysTargaryenIronThrone.PNG/revision/latest/top-crop/width/360/height/360?cb=20190520173137",
+        scores: [3, 2, 5, 1, 4, 2, 3, 4, 2, 1]
+    },
+    {
+        name: "Arya Stark",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/b/be/AryaShipIronThrone.PNG/revision/latest?cb=20190520174300",
+        scores: [3, 2, 5, 4, 1, 2, 3, 5, 5, 2]
+    },
+    {
+        name: "Tryion Lannister",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/9/95/HandoftheKingTyrionLannister.PNG/revision/latest?cb=20190520175204",
+        scores: [3, 5, 2, 4, 1, 2, 3, 5, 2, 1]
+    },
+    {
+        name: "Jon Snow",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Jon_Snow_Season_8.png/220px-Jon_Snow_Season_8.png",
+        scores: [3, 2, 5, 1, 4, 2, 3, 4, 2, 1]
+    },
+    {
+        name: "Brienne of Tarth",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a9/S8_Brienne_Profil.jpg/revision/latest?cb=20190423165941",
+        scores: [1, 2, 5, 3, 2, 5, 4, 1, 2, 5]
+    },
+    {
+        name: "Olenna Tyrell",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/f/f2/OlennaS7E3.png/revision/latest?cb=20170803074257",
+        scores: [2, 5, 4, 1, 2, 3, 5, 4, 2, 5]
+    }
+];
+
+module.exports = friends;
